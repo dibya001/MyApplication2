6085ac714eaa88a7ebe031ab6c237ed3b9409e1e
dibya001 <dibyamishra016@gmail.com>
2017-01-30 13:55:06 +0530

    added toolbar in the event location page

--- app/src/main/java/com/example/dibya/myapplication/EventLocation.java
+++ app/src/main/java/com/example/dibya/myapplication/EventLocation.java
@@ -1,11 +1,13 @@
 package com.example.dibya.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,6 +20,10 @@ public class EventLocation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_location);
         map= (ImageView) findViewById(R.id.map);
+        Toolbar tb= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        setTitle("Location");
+        tb.setTitleTextColor(Color.WHITE);
         TextView tx= (TextView) findViewById(R.id.loctext);
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Charter.ttf");
         tx.setTypeface(myTypeface);
--- app/src/main/java/com/example/dibya/myapplication/splashscreen.java
+++ app/src/main/java/com/example/dibya/myapplication/splashscreen.java
@@ -22,6 +22,6 @@ public class splashscreen extends AppCompatActivity {
                 startActivity(mainIntent);
                 finish();
             }
-        }, 3000);
+        }, 2000);
     }
 }
